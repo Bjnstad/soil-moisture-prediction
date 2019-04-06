b742eb338874daf464ee7915081c99a03bbf061b
Axel Bjørnstad <axebjo@gmail.com>
2019-04-06 15:22:41 +0200

    url loader

--- models/Station.py
+++ models/Station.py
@@ -11,12 +11,29 @@ class Station():
 class Weather(Station):
     def __init__( self, _id: str, _coord: Coord ):
         super().__init__(_id, _coord)
-
-    """
+        self._values = []
+   
+   
+   """
         Load raw data from database
     """
     def loadData(self):
-        self._values = []
+        try:
+            with urllib.request.urlopen(url5, timeout=120) as url2:
+                data = json.loads(url2.read().decode('utf-8'))
+                try:
+                    days = data['history']['days']
+                    for day in days:
+                        _date = datetime.datetime.fromtimestamp( day['summary']['date']['epoch'] ).strftime('%Y-%m-%d')
+                        res.append([  _date, day['summary']['precip'] ])
+                except:
+                    return False
+            return res          
+        except urllib.error.URLError as e:
+            print('--- failour ---')
+            print(e.reason)
+            return "failed"
+
 
 
 class SCAN(Station):
