eb01125250e528de86a866776e5d2f404fbab0f0
Axel Bjørnstad <axebjo@gmail.com>
2019-04-10 12:25:44 +0200

    added live raw data

--- models/Station.py
+++ models/Station.py
@@ -1,3 +1,5 @@
+import urllib.request, json, datetime
+
 class Coord():
     def __init__(self, lat, lng):
         self.lat = lat
@@ -7,40 +9,55 @@ class Station():
     def __init__( self, _id: str, _coord: Coord ):
         self._id = _id
         self._coord = _coord
+        self._values = []
+
+    def validateValues(self, start, end):
+        _delta = (end - start).days
+        _current = start
+        _i = 0
+        while _i < _delta:
+            if _i >= len(self._values):
+                _current = _current + datetime.timedelta(days=1)
+                _i += 1
+                self._values.append(None)
+                continue
+
+
+            # print( str(_i) + " - " + str(len(self._values)) ) 
+            _value = self._values[_i]
+            if _value == None:
+                self._values.append(None)
+            else:
+                self._values.insert(_i, None)    
+
+            _current = _current + datetime.timedelta(days=1)
+            _i += 1
+
+        print(_i)
 
 class Weather(Station):
     def __init__( self, _id: str, _coord: Coord ):
         super().__init__(_id, _coord)
-        self._values = []
+        self._url = "http://3.122.118.248/get.php?station=" + _id
 
-        _delta = 3650
-        for _index in range(0, _delta):
-            self._values.append(_index)
-       
-    """
-        Load raw data from database
-    """
-    def loadData(self):
-        pass
-    
-    """
+    def loadValues(self):
         try:
-            with urllib.request.urlopen(url5, timeout=120) as url2:
-                data = json.loads(url2.read().decode('utf-8'))
-                try:
-                    days = data['history']['days']
-                    for day in days:
-                        _date = datetime.datetime.fromtimestamp( day['summary']['date']['epoch'] ).strftime('%Y-%m-%d')
-                        res.append([  _date, day['summary']['precip'] ])
-                except:
-                    return False
-            return res          
+            with urllib.request.urlopen(self._url, timeout=120) as url:
+                data = json.loads(url.read().decode('utf-8'))
+                # Parse data
+                for day in data:
+                    self._values.append([
+                        datetime.datetime.strptime( day['date'], "%Y-%m-%d %H:%M:%S" ),
+                         day['value']
+                    ])
+        
         except urllib.error.URLError as e:
             print('--- failour ---')
             print(e.reason)
             return "failed"
-    """
-
+        except Exception as e:
+            print(e)
+    
 
 class SCAN(Station):
     def __init__( self, _id: str, _coord: Coord ):
--- models/Voronoi.py
+++ models/Voronoi.py
@@ -2,34 +2,17 @@ from scipy.spatial import Voronoi as V
 from shapely.geometry.polygon import Polygon
 from shapely.geometry.point import Point
 from models.Station import SCAN, Weather
+from dateutil.parser import parse
 import numpy as np
 import math
 import geopy.distance
 
 
 class Voronoi():
-    """
-    This create a Voronoi map of Scan and weather stations.
-    ...
-
-    Attributes
-    ----------
-    _scan : list[SCAN]
-        List of scan stations
-    _weather : list[Weather]
-        List of weather stations
-        _
-    Methods
-    -------
-    says(sound=None)
-        Prints the animals name and what sound it makes
-    """
-
-    _cells = []
-
     def __init__(self, _scans, _weather_stations):
         self._scan_stations = _scans
         self._weather_stations = _weather_stations
+        self._cells = []
         self.map_cells()
 
 
@@ -77,16 +60,6 @@ class Voronoi():
 
 
 class Cell():
-    """
-    This is an cell in a Vorronoi Tessolation map.
-    ...
-
-    Attributes
-    ----------
-    _soil : Soil
-        SCAN station that is relevant for the 
-    """
-
     def __init__(self, _scan: SCAN, _weather_stations=[]):
         if _weather_stations is None:
             _weather_stations = [Weather]
@@ -95,9 +68,10 @@ class Cell():
 
     def calculate(self):
         for station in self._weather_stations:
-            station.loadData()
+            station.loadValues()
+            station.validateValues( parse("2009-03-03"), parse("2019-03-03") )
 
-        _delta = 1
+        _delta = (parse("2009-03-03") - parse("2019-03-03")).days
 
         # Loop each day and add weighted average
         _weighted_average = []
@@ -108,36 +82,19 @@ class Cell():
         
         # TODO: Store values in databasee
 
-    """
-        Should download raw weather data and create an weighted average data and create an weighted average  
-    """
-
-    """
-        Calculate weighted average for one day.
-
-        day : int 
-            Is the index of the current day
-    """
-
     # TODO: Skip days with null value in measeure data 
     def calculate_weighted_average(self, day):
         _sum = 0  # Total sum of all stations
-        _max_distance = 10
         _total_weights = 0
 
         for station in self._weather_stations:
-            # TODO: Add value times distance from SCAN station to _sum
             scan_coords = [self._scan._coord.lat, self._scan._coord.lng]
             station_coords = [station._coord.lat, station._coord.lng]
             _distance = geopy.distance.distance(scan_coords, station_coords).km
-            # _distance = math.hypot(scan_coords.lng - scan_coords.lat, station_coords.lng - station_coords.lat)
 
-            print(_distance)
-            # TODO: we need to find weights
-            if _distance < _max_distance:
-                weight = _distance*(-(1/_max_distance))+1
-                _sum += station._value[day]*weight
-                _total_weights += 1
+            weight = 1/_distance
+            # _sum += station._value[day]*weight
+            _total_weights += weight
 
         if _total_weights > 0:
             return _sum / _total_weights
