3fb18648445c38b416ad2e613760391ffb8ed6e4
Axel Bjørnstad <axebjo@gmail.com>
2019-05-14 12:32:41 +0200

    added support for every station

--- Days.py
+++ Days.py
@@ -8,10 +8,16 @@ class Days:
         self._precip    = []
         self._moist_2   = []
 
-    def add_day(self, _date, _precip, _moist_2):
-        self._date.append( _date )
-        self._precip.append( _precip )
-        self._moist_2.append( _moist_2 )
+    def add_day(self, _index, _date, _precip, _moist_2):
+        if (_index +1 >= len(self._date) ):
+            self._date.append( [_date] )
+            self._precip.append( [_precip] )
+            self._moist_2.append( [_moist_2] )
+    
+        else:
+            self._date[_index].append( _date )
+            self._precip[_index].append( _precip )
+            self._moist_2[_index].append( _moist_2 )
     
     def get_date(self):
         return np.array(self._date)
@@ -33,3 +39,79 @@ class Days:
         for i in _tmp:
             _res.append(_tmp)
         return np.array(_res)
+
+
+    def get_data(self, _ratio):
+
+        # How many days need for prediction 
+        _pre = 3
+
+        _tx = [ [], [] ]
+        _ty = [ [] ]
+        _vx = [ [], [] ]
+        _vy = [ [] ]
+        
+        # Create a set for each station
+        for _in in range(0, len(self._date)):
+
+            """
+                We split the data into two parts, based on the ratio input given
+                in the function. The _ti variable is how many of the data values 
+                in each station will be used as testing while the vi variable
+                represents the testing values.
+            """
+            _ti = int( len(self._date[_in]) * _ratio )
+            _vi = int( len(self._date[_in]) * (1 - _ratio) )
+
+            # We always need 3 days
+            while (_ti % _pre != 0 ): _ti -= 1
+            while (_vi % _pre != 0 ): _vi -= 1
+
+            # Create testing set
+            for i in range( 0, int( (_ti / _pre) - _pre ) ):
+                _xb1 = []
+                _xb2 = []
+                _yb  = []
+
+                # Starting point
+                for k in range(i*_pre, i*_pre + _pre):
+                    _xb1.append( [self._moist_2[_in][k]] )
+                    _xb2.append( [self._precip[_in][k]] )
+                    _yb.append( [self._moist_2[_in][k +_pre]] )
+
+                _tx[0].append(_xb1)
+                _tx[1].append(_xb2)
+                _ty[0].append(_yb) # Validate on the next day moisture
+
+            # Create validation set
+            for i in range( _ti +1, int( _ti + (_vi / _pre) - _pre ) ):
+                _xb1 = []
+                _xb2 = []
+                _yb  = []
+
+                # Starting point
+                for k in range(i, i + _pre):
+                    _xb1.append( [self._moist_2[_in][k]] )
+                    _xb2.append( [self._precip[_in][k]] )
+                    _yb.append( [self._moist_2[_in][k +_pre]] )
+                    
+                _vx[0].append(_xb1)
+                _vx[1].append(_xb2)
+                _vy[0].append(_yb) 
+
+       
+        # Declare values
+        _train = {
+            'x':  _tx,
+            'y': _ty
+        }
+
+        _test = {
+            'x': _vx,
+            'y': _vy
+        }
+        
+        return {
+            'train': _train,
+            'test': _test
+        }
--- train.py
+++ train.py
@@ -1,9 +1,15 @@
 #!/usr/local/bin/python3.6
 
+import tensorflow as tf
+from tensorflow.python import debug as tf_debug
+
+import keras
 from keras.models import Model
 from keras.models import Sequential
 from keras.layers import Dense, Activation, LSTM, concatenate, Input, Flatten
 from keras.callbacks import TensorBoard
+
+import matplotlib.pyplot as plt
 
 import load
 
@@ -42,31 +48,38 @@ def build_model(data_length, label_length):
 
 
 if __name__ == '__main__':
+    # keras.backend.set_session(tf_debug.TensorBoardDebugWrapperSession(tf.Session(), "127.0.0.1:6011"))
 
+    # Load data
     _days = load.new()
+    _data = _days.get_data(.64)
+
+    _train = _data['train']
+    _test = _data['test']
+
 
     tensorboard = TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=False)
 
-    rnn = build_model( len(_days.get_date()), 1 )
-
-    rnn.fit(
-        [
-            _days.get_moist_2(),
-            _days.get_precip()
-        ],
-        [
-            _days.get_moist_2_labels()
-        ],
+    rnn = build_model( 3, 1 )
+    fit = rnn.fit(
+        _train['x'],
+        # _train['y'],
         validation_data=(
-            [
-                _days.get_moist_2(),
-                _days.get_precip()
-            ],
-            [
-                _days.get_moist_2_labels() 
-            ]
+            _test['x'],
+            _test['y']
         ),
-        epochs=20,
-        batch_size=32,
-        callbacks=[tensorboard]
+        epochs=5,
+        batch_size=15,
+        #        callbacks=[tensorboard]
     )
+
+    _predicted = []
+    _actual = []
+    for i in range( 0, 31):
+        _predict = rnn.predict([[_test['x'][0][i]], [_test['x'][1][i]]])
+        _actual.append( _test['y'][0][i][0] )
+        _predicted.append( _predict[0][0] )
+
+    plt.plot(_predicted, '-y')
+    plt.plot(_actual, '-r')
+    plt.show()
