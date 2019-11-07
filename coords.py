45fe8395ed70a43f43bf2b6eb47e17d89985fd02
Snorre Aas <saa@24sevenoffice.com>
2019-11-07 13:09:33 +0100

    Added load function for weather stations

--- coords.py
+++ coords.py
@@ -9,11 +9,10 @@ import pandas as pd
 
 class Coords:
 
-  def load(self):
+  def load_scan(self):
 
     station_csv = pd.read_csv("scan_stations.csv", sep=';')
     station_data = []
-    all_data = []
 
     for index, row in station_csv.iterrows():
 
@@ -31,5 +30,30 @@ class Coords:
     
     print(station_data)
 
+
+
+  def load_weather(self):
+
+      station_csv = pd.read_csv("weather_stations.csv", sep=';')
+      weather_station_data = []
+
+      for index, row in station_csv.iterrows():
+
+        weatherid = str(row[0])
+        lat = row[2]
+        lng = row[3]
+
+        weather_station_data.append({
+          "weatherStationid": weatherid,
+          "lat": lat,
+          "lng": lng
+        })
+      
+      print(weather_station_data)
+
+
+
+
 coords = Coords()
-coords.load()
+coords.load_weather()
+# coords.load_scan()
